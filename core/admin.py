20a904d72ff7c1277d0b6192aba71ecb329820b2
cristiansavaria <cristian2012sa@gmail.com>
2021-06-28 01:32:42 -0400

    eliminar

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Proveedor, Servicio
+from .models import Servicio
 
 # Register your models here.
 
 admin.site.register(Servicio)
-admin.site.register(Proveedor)
+
